f92bd8fd3360ce1f6973f8a9c64d998fd009c105
kaspernj <kaspernj@gmail.com>
2020-04-08 14:28:19 +0200

    Added missing defaults and added debug log

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Card = require("./card").default
 const Checkbox = require("./checkbox").default
 const Checkboxes = require("./checkboxes").default
 const Input = require("./input").default
-const Paginate = require("./paginate")
-const RadioButtons = require("./radio-buttons")
-const Select = require("./input")
-const SortLink = require("./sort-link")
+const Paginate = require("./paginate").default
+const RadioButtons = require("./radio-buttons").default
+const Select = require("./input").default
+const SortLink = require("./sort-link").default
+
+console.log({ AttributeRow, AttributeRows })
 
 module.exports = {
   AttributeRow,
